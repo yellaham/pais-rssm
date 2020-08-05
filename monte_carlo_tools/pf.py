d12fd1be7459efcda3b69595114967d312c0efec
Yousef El-Laham <yousef.ellaham@stonybrook.edu>
2020-08-05 13:41:25 -0400

    created base classes

--- monte_carlo_tools/pf.py
+++ monte_carlo_tools/pf.py
@@ -1,6 +1,6 @@
 class SSM:
     """
-    A class designed specifically for state-space models.
+    A class designed for state-space models.
     * Attributes
         - transition distribution (random number generation)
         - transition distribution (logarithm of the pdf)
@@ -12,3 +12,32 @@ class SSM:
         self.transition_log_pdf = trn_pdf
         self.observation_rand = obs_rand
         self.observation_log_pdf = obs_pdf
+    # TODO: Create an attribute for synthetic data generation
+    #
+
+
+class MultiRegimeSSM:
+    """
+    A class for regime switching state-space models. The attributes of the class include a list of different candidate
+    state-space models, as well as the distribution defining the dynamics of that regime-switching state-space model.
+    The pdf of the distribution defining the dynamics is also an attribute of the class object.
+    """
+    def __init__(self, regime_list, switch_rand, switch_lpdf):
+        self.regimes = regime_list
+        self.switching_dynamics_rand = switch_rand
+        self.switching_dynamics_lpdf = switch_lpdf
+    # TODO: Create an attribute for synthetic data generation
+    #
+
+
+def brspf(data, regimes, x_init, N=100):
+    """
+    Implementation of the bootstrap regime switching particle filter
+    :param data: [][] numpy array containing observations to be processed. Missing data is denoted by 'nan'.
+    :param regimes: object containing the candidate models for the regime switching
+    :param x_init: Initial particles used for propagation
+    :return ...
+    """
+    return 0
+
+
