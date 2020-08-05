b3d95320846ee20224f089514bfa7db8cbe67088
Yousef El-Laham <yousef.ellaham@stonybrook.edu>
2020-08-05 14:13:40 -0400

    added file for tracking example

---
+++ examples/ex1_switching_linear_ssm.py
@@ -0,0 +1,8 @@
+import numpy as np
+from monte_carlo_tools import pf
+
+## TODO: Finalize tracking example before moving on to analyzing the penguin data
+#       - Create SSM objects
+#       - Create regime switching model,
+#       - Generate synthetic data
+#       - Solve the tracking problem by running the bootstrap regime switching filter
--- monte_carlo_tools/pf.py
+++ monte_carlo_tools/pf.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class SSM:
     """
     A class designed for state-space models.
@@ -30,14 +33,45 @@ class MultiRegimeSSM:
     #
 
 
-def brspf(data, regimes, x_init, N=100):
+def brspf(data, regimes, x_init):
     """
     Implementation of the bootstrap regime switching particle filter
-    :param data: [][] numpy array containing observations to be processed. Missing data is denoted by 'nan'.
+    :param data: [dim_y][T] numpy array containing observations to be processed. Missing data is denoted by 'nan'.
     :param regimes: object containing the candidate models for the regime switching
-    :param x_init: Initial particles used for propagation
+    :param x_init: [dim_x][N] Initial particles used for propagation, where N is the number of particles
     :return ...
     """
+    # Determine relevant dimensions
+    dim_y = np.shape(data)[0]   # Dimension of each observation vector
+    dim_x = np.shape(x_init)[0]  # Dimension of each state vector
+
+    # Determine the time horizon
+    T = np.shape(data)[1]
+
+    # Determine the number of particles in the algorithm
+    N = np.shape(x_init)[1]
+
+    # Determine the number of models
+    K = len(regimes)
+
+    # Memory allocation for numpy arrays
+    x = np.zeros((T, dim_x, N))
+    m_idx = np.zeros((T, N))
+
+    for t in range(T):
+        # Step 1: Determine previous state
+        if t == 0:
+            x_old = x[t-1]
+        else:
+            x_old = x_init
+
+        # Step 2: Generate model indexes
+        m_idx[t] = regimes.switching_dynamics_rand(N)
+
+        # Step 3: Generate particles based on generated model indexes
+        for k in range(K):
+            print('help')
+
     return 0
 
 
