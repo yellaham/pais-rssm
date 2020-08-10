5474e7043ab7d2c48525d6db1d77281462e3d93e
Yousef El-Laham <yousef.ellaham@stonybrook.edu>
2020-08-10 12:54:01 -0400

    coded log of transition distribution and simplified methods

--- applications/penguins.py
+++ applications/penguins.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as sp
 
 class AgeStructuredModel:
     """
@@ -13,6 +14,9 @@ class AgeStructuredModel:
         self.variance_adults = var_s
         self.variance_chicks = var_c
         self.num_stages = nstage
+        logit_rates = self.reproductive_success_bias+self.reproductive_success_slope*np.linspace(0, self.num_stages-1,
+                                                                                              self.num_stages)
+        self.reproductive_rates = 1./(1.+np.exp(-logit_rates))
 
     def transition_rand(self, x_old):
         """
@@ -22,15 +26,12 @@ class AgeStructuredModel:
             - x[:, -J-2:] indicate the stage 1 to stage J-2 chicks
         :return an object with attributes S, Sb, C
         """
-        # Figure out how many things we are propagating
+        # Determine the number of samples
         num_samples = np.shape(x_old)[0]
         # Set up matrix to output everything
         x = np.zeros((num_samples, 2*self.num_stages-2))
-        # Compute the reproductive rate for each stage in logit space
-        logit_mu = self.reproductive_success_bias+self.reproductive_success_slope*np.linspace(0, self.num_stages-1,
-                                                                                              self.num_stages)
         # Obtain reproductive rate in real space by applying sigmoid transformation
-        pr = 1./(1.+np.exp(-logit_mu))
+        pr = self.reproductive_rates
         # Compute the total number of chicks
         ct_old = np.sum(x_old[:, -(self.num_stages-2):], axis=1)
         # From total number of chicks to state 1 adults
@@ -47,6 +48,30 @@ class AgeStructuredModel:
                 # Chicks obtained = binomial draw
                 x[:, self.num_stages+j-1] = np.random.binomial(x[:, j+1].astype(int), pr[j-1]).flatten()
         return x
+
+    def transition_log_pdf(self, x, x_old):
+        # Determine the number of samples
+        num_samples = np.shape(x)[0]
+        # Allocate array to story the evaluations of the transition distribution
+        log_transition = np.zeros(num_samples)
+        # Compute the total number of chicks
+        ct_old = np.sum(x_old[:, -(self.num_stages-2):], axis=1)
+        # From total number of chicks to state 1 adults
+        log_transition += sp.binom.logpmf(x[:, 0], ct_old, p=self.juvenile_survival)
+        # Remainder of cycle
+        for j in range(self.num_stages-1):
+            # Propagate adults first
+            if j < self.num_stages-2:
+                log_transition += sp.binom.logpmf(x[:, j+1], x_old[:, j].astype(int), p=self.adult_survival)
+            else:
+                log_transition += sp.binom.logpmf(x[:, j+1], (x_old[:, j] + x_old[:, j+1]).astype(int),
+                                                  p=self.adult_survival)
+            # Obtain the chicks for the penguins that can breed
+            if j >= 1:
+                log_transition += sp.binom.logpmf(x[:, self.num_stages+j-1], x[:, j+1].astype(int),
+                                                  p=self.reproductive_rates[j-1])
+        return log_transition
+
         ## TODO: We need to write the relevant methods of this class
         #   - Transition distribution (log pdf)
         #   - Observation distribution (random number generator)
--- examples/ex3_regime_switching_penguins.py
+++ examples/ex3_regime_switching_penguins.py
@@ -6,5 +6,11 @@ from monte_carlo_tools import pf
 penguin_model = penguins.AgeStructuredModel(psi_juv=0.4, psi_adu=0.9, alpha_r=0, beta_r=0.25, var_s=0.01, var_c=0.01,
                                             nstage=5)
 # Set a state value for testing
-xtest = 1000*np.ones((1, 8))
-print(penguin_model.transition_rand(xtest))
+xold = np.random.random_integers(low=500, high=2000, size=(50, 8))
+xnew = penguin_model.transition_rand(xold)
+
+# Print generated state
+print(xnew)
+
+# Evaluate log pdf
+print(penguin_model.transition_log_pdf(xnew, xold))
